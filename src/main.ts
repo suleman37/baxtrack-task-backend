f702d813d813d9db46f290d6680a843f984094ef
Suleman Shakeel <162445590+suleman37@users.noreply.github.com>
2026-05-08 12:30:14 +0500

    Enable CORS for local development in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
   const port = process.env.PORT || 8080;
   await app.listen(port);
   console.log(`Server running on Port ${port}`);
