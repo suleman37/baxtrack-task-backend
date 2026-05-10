81aca6eff71dc992dc82d48c109ccf462f940c61
Suleman Shakeel <162445590+suleman37@users.noreply.github.com>
2026-05-10 15:24:32 +0500

    allow  cors

--- src/common/constants/app.constants.ts
+++ src/common/constants/app.constants.ts
@@ -1,2 +1,5 @@
 export const DEFAULT_SERVER_PORT = 8080;
-export const DEFAULT_CORS_ORIGIN = 'http://localhost:3000';
+export const DEFAULT_CORS_ORIGINS = [
+  'http://localhost:3000',
+  'https://bxtrack-task.vercel.app',
+];
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,11 +1,11 @@
 import {
-  DEFAULT_CORS_ORIGIN,
+  DEFAULT_CORS_ORIGINS,
   DEFAULT_SERVER_PORT,
 } from '../common/constants/app.constants';
 
 export function getAppConfig() {
   return {
-    corsOrigin: DEFAULT_CORS_ORIGIN,
+    corsOrigin: DEFAULT_CORS_ORIGINS,
     port: process.env.PORT || DEFAULT_SERVER_PORT,
   };
 }
